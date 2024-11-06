2a18e8b146669ac9a367d63fe190083f56ebf62c
flooijdt <flooijdt@posteo.de>
2024-11-06 10:09:22 -0300

    testing integer overflow chap. 2 of book

--- overflow.c
+++ overflow.c
@@ -4,11 +4,12 @@ int main() {
   int var = 200 * 300 * 400 * 500;
   double varl = 200 * 300 * 400 * 500;
   long int varlong = 200 * 300 * 400 * 500;
-  // for printf:
-  //   u = int unsigned;
-  //   d = int signed; it is signed by default
-  //   l = long;
-  //   f = float and double.
+  /* for printf:
+    u = int unsigned;
+    d = int signed; it is signed by default
+    l = long;
+    f = float and double.
+  */
   printf("%u\n", var);
   return 0;
 }
